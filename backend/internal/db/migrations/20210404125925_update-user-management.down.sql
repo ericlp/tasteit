588bbde0c5efa953f897801d2a8f7ef4d27111a1
Eric Carlsson <cirecirecire3@gmail.com>
2022-02-26 11:03:57 +0100

    fix broken down migrations

--- backend/internal/db/migrations/20210404125925_update-user-management.down.sql
+++ backend/internal/db/migrations/20210404125925_update-user-management.down.sql
@@ -1,11 +1,11 @@
 ALTER TABLE tasteit_user
-    ADD COLUMN email TEXT NOT NULL DEFAULT '',
+    ADD COLUMN email    TEXT NOT NULL DEFAULT '',
     ADD COLUMN provider TEXT NOT NULL DEFAULT '';
 
 UPDATE tasteit_user
-SET tasteit_user.email = user_email.email,
-SET tasteit_user.provider = user_email.provider
+SET email    = user_email.email,
+    provider = user_email.provider
 FROM user_email
-WHERE tasteit.id = user_email.user_id;
+WHERE tasteit_user.id = user_email.user_id;
 
 DROP TABLE IF EXISTS user_email;
