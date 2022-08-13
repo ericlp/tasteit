d35b9dd443d9f58127fa4c24f347900cfd4c5edc
Eric Carlsson <cirecirecire3@gmail.com>
2022-08-13 21:18:47 +0200

    begin change frontend login things

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import { faFacebookF, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 import { Api, ApiResponse } from "../api/Api";
@@ -36,72 +34,18 @@ const Login = () => {
           <div className="errorText marginTop marginBottom">{error}</div>
         )}
 
-        {/* GITHUB */}
-        <Button
-          variant="primary"
-          size="large"
-          className={`${styles.signInButton} ${styles.githubButton}`}
-          onClick={() => {
-            handleLoginResponse(Api.user.githubLogin());
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            className={styles.signInButtonIcon}
-          />
-          {t.login.loginWithGithub}
-        </Button>
-
-        {/* FACEBOOK */}
-        <Button
-          variant="primary"
-          size="large"
-          className={`${styles.signInButton} ${styles.facebookButton}`}
-          onClick={() => {
-            handleLoginResponse(Api.user.facebookLogin());
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faFacebookF}
-            className={`${styles.signInButtonIcon} ${styles.facebookIcon}`}
-          />
-          {t.login.loginWithFacebook}
-        </Button>
-
-        {/* GOOGLE */}
-        <Button
-          variant="primary"
-          size="large"
-          className={`${styles.signInButton} ${styles.googleButton}`}
-          onClick={() => {
-            handleLoginResponse(Api.user.googleLogin());
-          }}
-        >
-          <div className={styles.signInButtonIcon}>
-            <Image
-              alt="Google"
-              src={"/icon_google.svg"}
-              layout={"responsive"}
-              width={"16px"}
-              height={"16px"}
-            />
-          </div>
-          {t.login.loginWithGoogle}
-        </Button>
-
-        {/* MICROSOFT */}
         <Button
           variant="primary"
           size="large"
           className={`${styles.signInButton} ${styles.microsoftButton}`}
           onClick={() => {
-            handleLoginResponse(Api.user.microsoftLogin());
+            handleLoginResponse(Api.user.login());
           }}
         >
           <div className={styles.signInButtonIcon}>
             <Image
               alt="Microsoft"
-              src={"/icon_microsoft.svg"}
+              src={"/itlogo.svg"}
               layout={"responsive"}
               width={"16px"}
               height={"16px"}
